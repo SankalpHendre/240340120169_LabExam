69790bb1d8891e7353c964187767df74f3f50d80
SankalpHendre <sankalphendre5281@gmail.com>
2024-07-31 16:17:38 +0530

    File added succesfully

---
+++ Even.java
@@ -0,0 +1,13 @@
+public class Even{
+    public static void main(String[] args) {
+		
+		int num=1;
+		//int result=0;
+		for(num=1;num<=20;num++) {
+			if(num % 2== 0) {
+				System.out.print(num+" ");
+			}
+		}
+
+	}
+}
